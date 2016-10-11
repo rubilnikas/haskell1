53af113718b6d4d3d86fd928bfe782725e4e2d33
Mindaugas Mažalskis <mindaugas.mazalskis@adform.com>
2016-10-11 17:08:46 +0300

    use list comprehension

--- src/TicTacToe/Board.hs
+++ src/TicTacToe/Board.hs
@@ -2,6 +2,5 @@ module TicTacToe.Board where
 import TicTacToe.Move.Move
 
 getEmptyBoard:: [Move]
-getEmptyBoard = [Move 0 0 N, Move 0 1 N, Move 0 2 N, Move 1 0 N, Move 1 1 N, Move 1 2 N, Move 2 0 N, Move 2 1 N, Move 2 2 N]
-
+getEmptyBoard = [Move x y N | x <- [0 .. 2], y <- [0 .. 2]]
 
